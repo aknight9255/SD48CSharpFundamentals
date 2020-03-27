6c205d1ecc1933bbb577e48dfc518b90721418cc
aknight9255 <amanda.knight55@gmail.com>
2020-03-27 10:20:17 -0400

    Friday Morning

--- 10_Interfaces_WorkingWithDI/TransactionTests.cs
+++ 10_Interfaces_WorkingWithDI/TransactionTests.cs
@@ -20,7 +20,25 @@ namespace _10_Interfaces_WorkingWithDI
         [TestMethod]
         public void PayDebtTest()
         {
+            PayDebt(new Dollar());
+            PayDebt(new ElectronicPayment(4000.32m));
+            decimal expectedDebt = 9000.12m - 4001.32m;
+            Assert.AreEqual(expectedDebt, _debt);
+        }
 
+        [TestMethod]
+        public void InjectingIntoConstructors()
+        {
+            var dollar = new Dollar();
+            var epayment = new ElectronicPayment(1.50m);
+            var firstTransaction = new Transaction(dollar);
+            var secondTransaction = new Transaction(epayment);
+
+            Console.WriteLine(firstTransaction.GetTransactionType());
+            Console.WriteLine(secondTransaction.GetTransactionType());
+
+            Assert.AreEqual("Dollar", firstTransaction.GetTransactionType());
+            Assert.AreEqual(1.50m, secondTransaction.GetTransactionAmount());
         }
     }
 }
