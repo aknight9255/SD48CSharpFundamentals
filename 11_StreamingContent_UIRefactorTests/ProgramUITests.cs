3169b14afaa3c757daf460bcffbaf9d894fd77aa
aknight9255 <amanda.knight55@gmail.com>
2020-03-30 16:27:24 -0400

    Monday afternoon notes

--- 11_StreamingContent_UIRefactorTests/ProgramUITests.cs
+++ 11_StreamingContent_UIRefactorTests/ProgramUITests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using _11_StreamingContent_UIRefactor;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace _11_StreamingContent_UIRefactorTests
@@ -7,8 +9,31 @@ namespace _11_StreamingContent_UIRefactorTests
     public class ProgramUITests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetList_OutPutShouldContainList()
         {
+            //Arrange 
+            List<string> commandList = new List<string> { "1", "5" };
+            MockConsole console = new MockConsole(commandList);
+            ProgramUI program = new ProgramUI(console);
+
+            //ACT
+            program.Run();
+            Console.WriteLine(console.Output);
+
+            //ASSERT 
+            Assert.IsTrue(console.Output.Contains("Princess Bride"));
+        }
+        [TestMethod]
+        public void AddToList_ShouldSeeItemInList()
+        {
+            //Arrange 
+            var commandList = new List<string> { "3", "This is a test", "22", "4", "false", "1234,01,24", "1","5" };
+            var console = new MockConsole(commandList);
+            var ui = new ProgramUI(console);
+            //ACT
+            ui.Run();
+            Console.WriteLine(console.Output);
+            Assert.IsTrue(console.Output.Contains("This is a test"));
         }
     }
 }
